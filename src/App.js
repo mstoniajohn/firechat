6ecd813a3401f358047dce9a3997137a74bd5c3c
github username <github email address>
2021-03-12 05:05:55 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 firebase.initializeApp({
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: 'all-chat-b5c6f',
+	projectId: process.env.REACT_APP_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
